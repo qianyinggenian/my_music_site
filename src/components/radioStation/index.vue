abd7b79253ca97fb66f1d7abd4c7490dc4099f17
qianyinggenian <1368504885@qq.com>
2021-06-03 23:22:55 +0800

    更新

--- src/components/radioStation/index.vue
+++ src/components/radioStation/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content">
+  <div class="container">
     <div class="carousel">
       <el-carousel :interval="3000" type="card" height="200px">
         <el-carousel-item v-for="(item,index) in banners" :key="index">
@@ -10,6 +10,17 @@
         </el-carousel-item>
       </el-carousel>
     </div>
+      <div class="middle"></div>
+      <div class="bottom">
+          <div class="title">
+              听听 <i class="el-icon-right"></i>
+          </div>
+          <div class="content">
+              <div class="info">
+                  <img src="" alt="">
+              </div>
+          </div>
+      </div>
   </div>
 </template>
 
@@ -24,12 +35,15 @@
       return {
         carouselType: 'card',
         carouselHeight: '230px',
-        banners: [], // 轮播图信息
+        banners: [], // 轮播图信息，
+        personalizeList: [], // 电台个性化推荐,
+        djRadios: [], // 创作翻唱
       };
     },
     mounted() {
       this.getBanner();
       this.hotFn();
+      this.getInfo();
     },
     methods: {
       /**
@@ -51,13 +65,64 @@
       */
       async hotFn () {
         const { data: data } = await this.$axios.get('/dj/catelist');
-      }
+      },
+        async getInfo () {
+          this.getPersonalizeRecommendFn();
+          this.getPersonalizeRecommendFn1();
+          this.getPersonalizeRecommendFn2();
+          this.getPersonalizeRecommendFn3();
+          this.getPersonalizeRecommendFn4();
+          this.getPersonalizeRecommendFn5();
+        },
+        // 电台个性化推荐
+        async getPersonalizeRecommendFn () {
+          const { data } = await this.$axios.get('/dj/personalize/recommend');
+          if (data.code === 200) {
+              this.perpersonalizeList = data.data;
+          }
+        },
+        // 创作翻唱
+        async getPersonalizeRecommendFn1 () {
+            const { data } = await this.$axios.get('/dj/radio/hot?cateId=2001');
+            if (data.code === 200) {
+                this.djRadios = data.djRadios;
+            }
+        },
+        // 情感调频
+        async getPersonalizeRecommendFn2 () {
+            const { data5 } = await this.$axios.get('/dj/radio/hot?cateId=3');
+            if (data.code === 200) {
+                this.djRadios = data.djRadios;
+            }
+        },
+        // 声音恋人
+        async getPersonalizeRecommendFn3 () {
+            const { data2 } = await this.$axios.get('/dj/radio/hot?cateId=3001');
+            if (data.code === 200) {
+                this.djRadios = data.djRadios;
+            }
+        }
+        ,
+        // 声之剧场
+        async getPersonalizeRecommendFn4 () {
+            const { data2 } = await this.$axios.get('/dj/radio/hot?cateId=10001');
+            if (data.code === 200) {
+                this.djRadios = data.djRadios;
+            }
+        },
+        // 音乐故事
+        async getPersonalizeRecommendFn5 () {
+            const { data5 } = await this.$axios.get('/dj/radio/hot?cateId=2');
+            if (data.code === 200) {
+                this.djRadios = data.djRadios;
+            }
+        },
     }
   }
 </script>
 
 <style lang="less" scoped>
-.content {
+.container {
   margin: 0 60px;
   .carousel {
     img {
@@ -78,5 +143,25 @@
       padding: 2px;
     }
   }
+    .middle {
+        width: 100%;
+        height: 150px;
+        background-color: #ec4141;
+    }
+    .bottom {
+        .title {
+            margin: 10px 0;
+            height: 30px;
+        }
+        .content {
+            height: 250px;
+            .info {
+                img {
+                    width: 200px;
+                    height: 200px;
+                }
+            }
+        }
+    }
 }
 </style>
